1d2c1df114aaca67197e217cafa31b1677e0d700
TsSyed <ssn250@gmail.com>
2026-01-29 22:11:02 +0800

    Edit Kod App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* global __firebase_config, __app_id, __initial_auth_token */
 import React, { useState, useEffect, useMemo } from 'react';
 import { initializeApp } from 'firebase/app';
 import { 
